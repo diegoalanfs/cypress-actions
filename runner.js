c9c28b81296b8330bd652b0d5d04099e0b60a162
Diego Alan <48769923+diegoalanfs@users.noreply.github.com>
2024-08-15 15:48:46 -0300

    Update runner.js

--- runner.js
+++ runner.js
@@ -1,14 +1,14 @@
 const cypress = require('cypress')
 const tesults = require('cypress-tesults-reporter');
 
-const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjFlZGRlNDlhLTJiMWEtNDZkMS1hMzMzLTdlY2E4YzI2M2Y2Zi0xNzIzNzQyMTg1MDM5IiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiMWFmMmU2ZTktMGMwYy00YmYyLTk0MzUtNGYzMTFhN2M3NmVhIiwidHlwZSI6InQifQ.-wTz82eYueSyCgx_Ur-qAH1bkmikeQlk6E3j7gCpkgU'
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjFlZGRlNDlhLTJiMWEtNDZkMS1hMzMzLTdlY2E4YzI2M2Y2Zi0xNzIzNzQyMTg1MDM5IiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiMWFmMmU2ZTktMGMwYy00YmYyLTk0MzUtNGYzMTFhN2M3NmVhIiwidHlwZSI6InQifQ.-wTz82eYueSyCgx_Ur-qAH1bkmikeQlk6E3j7gCpkgU'
 
 cypress.run({
   // specs to run here
 })
 .then((results) => {
   const args = {
-    target: TOKEN,
+    target: token,
   }
   tesults.results(results, args);
 })
